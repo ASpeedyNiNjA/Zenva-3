41b27c0a32e1bf1ec31a492b985ada439e0daf82
Chris Trombly <christrombly001@hotmail.com>
2026-01-07 18:33:19 -0500

    Creating the Level

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,43 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rig;
     public float jumpForce;
+    public SpriteRenderer sr;
+
+    private bool isGrounded;
 
     void FixedUpdate()
     {
         //Player Movement Code
         float moveInput = Input.GetAxisRaw("Horizontal");
         rig.linearVelocity = new Vector2 (moveInput * moveSpeed, rig.linearVelocityY);
+
+        //Sprite Direction Pointing Code
+        if (rig.linearVelocityX > 0)
+        {
+            sr.flipX = true;
+        }
+        else if (rig.linearVelocityX < 0)
+        {
+            sr.flipX = false;
+        }
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded == true)
         {
+            isGrounded = false;
             rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.GetContact(0).normal == Vector2.up)
+        {
+            isGrounded = true;
         }
     }
 }
