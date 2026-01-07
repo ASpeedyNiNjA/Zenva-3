d83020e8fc6df7f81bf2d8611bd42b1519a92121
Chris Trombly <christrombly001@hotmail.com>
2026-01-07 17:40:51 -0500

    Player Jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,20 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rig;
+    public float jumpForce;
 
     void FixedUpdate()
     {
         //Player Movement Code
         float moveInput = Input.GetAxisRaw("Horizontal");
         rig.linearVelocity = new Vector2 (moveInput * moveSpeed, rig.linearVelocityY);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
