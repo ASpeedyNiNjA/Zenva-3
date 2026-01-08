820edc7abb8c727de7b258982b219a721e6398a5
Chris Trombly <christrombly001@hotmail.com>
2026-01-07 20:29:25 -0500

    Game Over - Part 2

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -31,9 +31,14 @@ public class Enemy : MonoBehaviour
             {
                 targetPos = startPos;
             }
+        }   
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerController>().GameOver();
         }
-    
-    
-    
     }
 }
