7b6daa3cb89f30ebcd34d9f1203b953f9ebd9efa
Chris Trombly <christrombly001@hotmail.com>
2026-01-07 20:10:51 -0500

    Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector3 moveOffset;
+    private Vector3 startPos;
+    private Vector3 targetPos;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position;
+        targetPos = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        
+        if(transform.position == targetPos)
+        {
+
+            if(targetPos == startPos)
+            {
+                targetPos = startPos + moveOffset;
+            }
+            else
+            {
+                targetPos = startPos;
+            }
+        }
+    
+    
+    
+    }
+}
